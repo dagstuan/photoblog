b30a4bb6e10a528ae62a19a4f3bb8e24420022f2
Dag Stuan <D.Stuan@gmail.com>
2012-08-23 15:21:37 +0200

    removing files upon deletion of post

--- photos/admin.py
+++ photos/admin.py
@@ -10,9 +10,28 @@ class PhotoInline(admin.TabularInline):
     model = Photo
 
 class PostAdmin(admin.ModelAdmin):
+    actions=['really_delete_selected']
+    
     fieldsets = [
         (None,               {'fields': ['title', 'pub_date', 'comment']}),
     ]
     inlines = [PhotoInline]
+    
+    def get_actions(self, request):
+        actions = super(PostAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+            
+    def really_delete_selected(self, request, queryset):
+        for obj in queryset:
+            obj.delete()
+
+        if queryset.count() == 1:
+            message_bit = "1 post entry was"
+        else:
+            message_bit = "%s post entries were" % queryset.count()
+        self.message_user(request, "%s successfully deleted." % message_bit)
+    
+    really_delete_selected.short_description = "Delete selected entries"
 
 admin.site.register(Post, PostAdmin)
