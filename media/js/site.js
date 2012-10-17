1ed92aac5663fc6b9cc5904e704f78ff4b5fcfd5
Dag Stuan <D.Stuan@gmail.com>
2012-10-17 10:37:31 +0200

    added google analytics

--- media/js/site.js
+++ media/js/site.js
@@ -24,21 +24,37 @@ var opts = {
 
 
 $(document).ready(function() {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-35576492-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    
     History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
-            var State = History.getState(); // Note: We are using History.getState() instead of event.state
-            ajax_url = '/post_ajax';
+        var State = History.getState(); // Note: We are using History.getState() instead of event.state
+        
+        ajax_url = '/post_ajax';
+        
+        if (State.data.url !== undefined) {                
+            ajax_url += State.data.url;
             
-            if (State.data.url !== undefined) {                
-                ajax_url += State.data.url;
-            }
-            
-            $.ajaxSetup({cache: false});
-                 
-            $.getJSON(ajax_url, {format: 'json'}, function(res) {
-                replacePhoto(res);
-            });
-            
+            _gaq.push(['_trackPageview', State.data.url]);
+        }
+        else {
+            _gaq.push(['_trackPageview', '/']);
+        }
+        
+        $.ajaxSetup({cache: false});
+             
+        $.getJSON(ajax_url, {format: 'json'}, function(res) {
+            replacePhoto(res);
         });
+        
+    });
     
     current_photo = $('#current_photo');
     
