fb8a227d92482b875b3339630b956963c70c0eac
Dag Stuan <D.Stuan@gmail.com>
2012-10-17 10:49:09 +0200

    apparently putting GA in an external js file is nono.. trying again..

--- media/js/site.js
+++ media/js/site.js
@@ -21,16 +21,6 @@ var opts = {
   left: 'auto' // Left position relative to parent in px
 }
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-35576492-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 $(document).ready(function() {
     
     
