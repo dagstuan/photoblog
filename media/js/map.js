b28c6eaad4c1e722277e3348fa23b9d50e685f84
Dag Stuan <dag.stuan@bekk.no>
2013-12-05 19:04:00 +0100

    forgot to remove reference to google maps integration

--- media/js/map.js
+++ media/js/map.js
@@ -152,9 +152,6 @@
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
             maxZoom: 18
         }).addTo(map);
-        
-        var gmap_layer = new L.Google('HYBRID');
-        map.addLayer(gmap_layer);
 
         var markers = L.markerClusterGroup({
             iconCreateFunction: function(cluster) {
